e200907b2c133eb6d97ce34e6403d5bb8c8737c6
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-03-31 17:19:22 +0200

    Add SalePricesController inheritance from ResourceController

--- app/controllers/spree/admin/sale_prices_controller.rb
+++ app/controllers/spree/admin/sale_prices_controller.rb
@@ -1,18 +1,14 @@
 module Spree
   module Admin
-    class SalePricesController < BaseController
+    class SalePricesController < ResourceController
 
       before_filter :load_product
 
       respond_to :js, :html
 
-      def index
-        @sale_prices = @product.sale_prices
-      end
-
       def create
-        @sale_price = @product.put_on_sale params[:sale_price][:value], sale_price_params
-        respond_with(@sale_price)
+        @sale_price = @product.put_on_sale(sale_price_amount, sale_price_params)
+        redirect_to admin_product_sale_prices_path(@product)
       end
 
       def destroy
@@ -26,6 +22,10 @@ module Spree
       def load_product
         @product = Spree::Product.find_by(slug: params[:product_id])
         redirect_to request.referer unless @product.present?
+      end
+
+      def sale_price_amount
+        params[:sale_price][:value].present? ? params[:sale_price][:value] : 0
       end
 
       def sale_price_params
