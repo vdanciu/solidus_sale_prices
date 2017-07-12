f018a7fabd9b0ff21720217ac2e12a8eed41919f
Alberto Vena <kennyadsl@gmail.com>
2017-07-12 12:23:22 +0200

    Always run migrations installing

--- lib/generators/solidus_sale_prices/install/install_generator.rb
+++ lib/generators/solidus_sale_prices/install/install_generator.rb
@@ -7,12 +7,7 @@ module SolidusSalePrices
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
-           run 'bundle exec rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
+        run 'bundle exec rake db:migrate'
       end
     end
   end
