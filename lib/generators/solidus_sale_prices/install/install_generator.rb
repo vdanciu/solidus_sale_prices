a4947afa3e7fd8c2f62ceb0854cde09fddff2c2e
Daniele Palombo <daniele.palombo@gmail.com>
2017-07-31 14:32:36 +0200

    Enject assets to install generator

--- lib/generators/solidus_sale_prices/install/install_generator.rb
+++ lib/generators/solidus_sale_prices/install/install_generator.rb
@@ -1,6 +1,15 @@
 module SolidusSalePrices
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      def add_javascripts
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_sale_prices\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_sale_prices\n"
+      end
+
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_sale_prices\n", before: /\*\//, verbose: true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_sale_prices\n", before: /\*\//, verbose: true
+      end
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=solidus_sale_prices'
