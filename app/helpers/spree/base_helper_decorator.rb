a4ebfa88dfec8594cbaa3a8d9fd573d6c24dc2c2
Daniele Palombo <ksemuldie@gmail.com>
2019-06-28 15:49:55 +0200

    Clean up code

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,15 +2,13 @@ Spree::BaseHelper.class_eval do
   def display_original_price(product_or_variant)
     product_or_variant.original_price_in(_current_currency).display_price.to_html
   end
-  
+
   def display_discount_percent(product_or_variant, append_text = 'Off')
     discount = product_or_variant.discount_percent_in _current_currency
-    
-    if discount > 0
-      return "#{number_to_percentage(discount, precision: 0).to_html} #{append_text}"
-    else
-      return ''
-    end 
+
+    return "#{number_to_percentage(discount, precision: 0).to_html} #{append_text}" if discount > 0
+
+    ''
   end
 
   # Check if a sale is the current sale for a product, returns true or false
@@ -22,9 +20,10 @@ Spree::BaseHelper.class_eval do
     try(:supported_currencies) || [ _current_currency ]
   end
 
-  private
-    def _current_currency
-      try(:current_currency) || Spree::Config[:currency] || 'USD'
-    end
 
+  private
+
+  def _current_currency
+    try(:current_currency) || Spree::Config[:currency] || 'USD'
+  end
 end
