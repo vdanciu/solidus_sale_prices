c4f744d08e6908a4f638962c5ba969441da93854
Daniele Palombo <ksemuldie@gmail.com>
2019-07-19 12:49:31 +0200

    Add flatpicker initialize code
    From solidus 2.5 jquery-datepicker has been replaced with `flatpicker`.
    This code is required to add the ability to set also the time.
    
    The selector will be the same, so no other changes are required in
    the code.

--- app/assets/javascripts/spree/backend/solidus_sale_prices.js
+++ app/assets/javascripts/spree/backend/solidus_sale_prices.js
@@ -1,31 +1,3 @@
-//= require jquery-ui-timepicker-addon
-
-SpreeSalePrices = {
-  handleDatetimePickerFields: function() {
-    $('.datetimepicker').datetimepicker({
-      dateFormat: Spree.translations.date_picker.js_format,
-      timeFormat: "hh:mm tt",
-      dayNames: Spree.translations.abbr_day_names,
-      dayNamesMin: Spree.translations.abbr_day_names,
-      firstDay: Spree.translations.first_day,
-      monthNames: Spree.translations.month_names,
-      prevText: Spree.translations.previous,
-      nextText: Spree.translations.next,
-      showOn: "focus",
-      oneLine: true
-    });
-  },
-
-  handleSelect2Fields: function() {
-    $('.variant_sales_picker').select2();
-  },
-
-  init: function() {
-    SpreeSalePrices.handleDatetimePickerFields();
-    SpreeSalePrices.handleSelect2Fields();
-  }
-}
-
 $(document).ready(function() {
-  SpreeSalePrices.init();
+  $('.variant_sales_picker').select2();
 });
